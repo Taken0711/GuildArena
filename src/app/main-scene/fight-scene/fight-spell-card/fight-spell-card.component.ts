d2287b2610d98d83ce8d01fe7df480690f881b1c
Jeremy Junac <jeremy.junac@etu.unice.fr>
2017-10-04 12:01:15 +0200

    spell selection much cleaner

--- src/app/main-scene/fight-scene/fight-spell-card/fight-spell-card.component.ts
+++ src/app/main-scene/fight-scene/fight-spell-card/fight-spell-card.component.ts
@@ -2,6 +2,12 @@ import {Component, Input, OnInit} from '@angular/core';
 import {SpellModel} from "../../../../shared/models/SpellModel";
 import {FightService} from "../../../../shared/services/fight/fight.service";
 
+const enum SpellState {
+  AVAILABLE,
+  SELECTED,
+  COOLDOWN,
+}
+
 @Component({
   selector: 'app-fight-spell-card',
   templateUrl: './fight-spell-card.component.html',
@@ -10,25 +16,26 @@ import {FightService} from "../../../../shared/services/fight/fight.service";
 export class FightSpellCardComponent implements OnInit {
 
   @Input() public spell: SpellModel;
-  public selected: boolean;
-  public borderStyle: string;
+  public state: SpellState = SpellState.AVAILABLE;
 
   constructor(private fightService: FightService) { }
 
   ngOnInit() {
     this.fightService.currentSelectedSpell$.subscribe(spell => {
       if (spell === this.spell) {
-        this.selected = true;
-        this.borderStyle = 'solid';
+        this.state = SpellState.SELECTED;
       } else {
-        this.selected = false;
-        this.borderStyle = 'none';
+        this.state = SpellState.AVAILABLE;
       }
     });
   }
 
-  selectSpell() {
+  public selectSpell(): void {
     this.fightService.updateCurrentSelectedSpell(this.spell);
+  }
+
+  public isSelected(): boolean {
+    return this.state === SpellState.SELECTED;
   }
 
 }
